cbfd1b5f5fbaa860408b8cb9d41fddd48d4986ad
osamarafa1994 <osama@motaweron.com>
2023-07-02 15:05:03 +0300

    college_enrollment_certificate

--- resources/views/admin/layouts/main-sidebar.blade.php
+++ resources/views/admin/layouts/main-sidebar.blade.php
@@ -158,10 +158,10 @@
                 <span class="side-menu__label">{{ trans('admin.re_record_the_track') }}
             </a>
         </li>
- <li class="slide">
-            <a class="side-menu__item" href="{{ route('re_record_the_track.index') }}">
+        <li class="slide">
+            <a class="side-menu__item" href="{{ route('certificates.registeration') }}">
                 <i class="fa fa-book side-menu__icon"></i>
-                <span class="side-menu__label">{{ trans('admin.re_record_the_track') }}
+                <span class="side-menu__label">{{ trans('admin.college_enrollment_certificate') }}
             </a>
         </li>
                 <li class="slide">
