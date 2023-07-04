28af39d89bb38d16930801e816fb13c6769c829f
eldapour <abdallahmahmoud1422003@gmail.com>
2023-07-04 11:22:07 +0300

    fix a bug in database

---
+++ resources/views/admin/certificates/print_certificate.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY FACULTY</title>
+    <link href="photo/logo.png" rel="icon" />
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="{{asset('certificate_assets')}}/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('certificate_assets')}}/css/bootstrap.min.css">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="{{asset('certificate_assets')}}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{asset('certificate_assets')}}/css/owl.theme.default.min.css">
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900;1000&family=Jost:wght@200;300;400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <style>
+        .border1{
+            border: 1px solid #618597;
+        }
+        .border2{
+            border: 15px solid #618597;
+            margin:2px;
+        }
+        .border3{
+            border: 1px solid #618597;
+            margin:2px;
+        }
+        .image-logo1{
+            display: flex;
+            justify-content: center;
+            margin-bottom: 20px;
+        }
+        .image-logo1 img{
+            height: 30px;
+        }
+        .table{
+            border: 1px solid black;
+        }
+        .border-color{
+            border: 1px solid black;
+            width: 40%;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+
+<div class="section">
+    <div class="container">
+        <div class="border1">
+            <div class="border2">
+                <div class="border3">
+                    <div class="p-5">
+                        <div class="image-logo1">
+                            <img src="photo/logo.png">
+                        </div>
+                        <h3 class="text-center mb-2">شهادة التسجيل بالكلية</h3>
+                        <p class="text-center">رقم الشهادة</p>
+                        <h5 class="mb-4">يشهد عميد الكلية 'اسم الكلية' أن الطالب:</h5>
+                        <div class="row">
+                            <div class="col-lg-9 col-12">
+                                <table class="table">
+                                    <tbody>
+                                    <tr>
+                                        <td class="border-color">الاسم الكامل</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="border-color">رقم التسجيل</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="border-color">رقم أبووجي</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="border-color">الرقم الوطني</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="border-color">رقم البطاقة الوطنية/جواز السفر</td>
+                                        <td></td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="col-lg-3 col-12 d-flex justify-content-center">
+                                <img src="photo/download.png" style="height: 170px;">
+                            </div>
+                        </div>
+                        <div class="mb-2">
+                            <span class="fs-6 fw-bold">مسجل بالكلية:</span>
+                            <span>'اسم الكلية'</span>
+                            <span class="fs-6 fw-bold">,برسم الموسم الجامعي: </span>
+                            <span>'الموسم الجامعي'.</span>
+                        </div>
+                        <div class="mb-3">
+                            <span class="fs-6 fw-bold"> بالمسلك الدراسي:</span>
+                            <span>' اسم المسلك'</span>
+                        </div>
+                        <table class="table">
+                            <tbody>
+                            <tr>
+                                <td class="fw-bold">الوحدات المسجل بها </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td class="border-color">اسم الوحدة</td>
+                                <td class="fw-bold">الفصل الدراسي</td>
+                            </tr>
+                            <tr>
+                                <td class="border-color"> الوحدة 1</td>
+                                <td>S1</td>
+                            </tr>
+                            <tr>
+                                <td class="border-color"> الوحدة 1</td>
+                                <td>S1</td>
+                            </tr>
+                            <tr>
+                                <td class="border-color"> الوحدة 1</td>
+                                <td>S1</td>
+                            </tr>
+                            <tr>
+                                <td class="border-color"> الوحدة 1</td>
+                                <td>S1</td>
+                            </tr>
+                            <tr>
+                                <td class="border-color"> الوحدة 1</td>
+                                <td>S1</td>
+                            </tr>
+                            <tr>
+                                <td class="border-color"> الوحدة 1</td>
+                                <td>S1</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <p class="mt-4 mb-3">سلمت هذه الشهادة الى المعني بالامر لتقديمها عند الحاجة</p>
+                        <div class="d-flex justify-content-end">
+                            <div>ختم المؤسسة</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="{{asset('certificate_assets')}}/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('certificate_assets')}}/js/all.min.js"></script>
+</body>
+</html>
