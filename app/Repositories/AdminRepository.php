bc2dd76f924d33f5a1a5a19c240f6dc7fbbdb195
osamarafa1994 <osama@motaweron.com>
2023-08-27 10:02:20 +0300

    with make repositoy

---
+++ app/Repositories/AdminRepository.php
@@ -0,0 +1,7 @@
+<?php namespace App\Repositories;
+
+use App\Interfaces\AdminRepositoryInterface;
+
+class AdminRepository implements AdminRepositoryInterface {
+    // Implement repository methods here
+}
---
+++ app/Repositories/SubjectRepository.php
@@ -0,0 +1,7 @@
+<?php namespace App\Repositories;
+
+use App\Repositories\SubjectRepositoryInterface;
+
+class EloquentSubjectRepository implements SubjectRepositoryInterface {
+    // Implement repository methods here
+}
